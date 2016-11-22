00475c3b89a8e819cb6b23a717b922d619241b5d
Bruno B. Scholl <bruno.bbs3@hotmail.com>
2016-11-22 12:00:26 -0200

    comeca as funcoes de jogada e velor de proximasw

--- hex.c
+++ hex.c
@@ -12,11 +12,42 @@ int **create_table()
     if (table != NULL)
         for (i = 0; i < 14; i++)
         {
-            table[i] = malloc(14 * sizeof(int));
+            table[i] = calloc(14 * sizeof(int));
             if (table[i] == NULL)
                 return NULL;
         }
     return table;
+}
+
+int mmiterate(int alpha, int beta, int max)
+{
+}
+
+void check_neighbor(int **table, int **board, int l, int c, int color, int val)
+{
+    if (board[i][j] == color)
+    {
+    }
+    return;
+}
+
+int play_value(int **board, int color)
+{
+    int **aux, i, j;
+    tab = create_table();
+    for (i = 0; i < 14; 1 ++)
+    {
+        for (j = 0; j < 14; j++)
+        {
+            if (board[i][j] == 0 && aux[i][j] == 0)
+                aux[i][j] = i + 1;
+            else if (board[i][j] == color)
+            {
+                aux[i][j] = i + 1;
+                check_negbor(aux, board, i, j, color, i + 1);
+            }
+        }
+    }
 }
 
 int main(int argc, char *argv[])
@@ -34,4 +65,6 @@ int main(int argc, char *argv[])
         if (argv[2][0] == 'd')
             dbg = 1;
     table = create_table();
+
+    while ()
 }
